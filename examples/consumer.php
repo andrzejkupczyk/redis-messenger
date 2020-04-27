c0314377135b4e08b050ea81538eca77f9a00400
Andrzej Kupczyk <kontakt@andrzejkupczyk.pl>
2020-04-27 14:23:01 +0200

    ✨ Use event dispatcher to handle incoming entries

--- examples/consumer.php
+++ examples/consumer.php
@@ -3,6 +3,8 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use WebGarden\Messaging\Client;
+use WebGarden\Messaging\Reader;
+use WebGarden\Messaging\Redis\Entry;
 use WebGarden\Messaging\Redis\Stream;
 
 $streamNames = explode(',', $argv[1] ?? 'stream');
@@ -13,4 +15,12 @@ $streams = array_map(function ($name) {
     return new Stream($name);
 }, $streamNames);
 
-$client->from(...$streams)->follow($from);
+$client
+    ->from(...$streams)
+    ->on(Reader::TIMEOUT_REACHED, function ($seconds) {
+        printf("Idle running for %01.3f seconds\n", $seconds);
+    })
+    ->on(Reader::ITEM_RECEIVED, function (Entry $entry, Stream $stream) {
+        printf("Received item %s from %s\n", $entry->id(), $stream);
+    })
+    ->await();
--- src/Reader.php
+++ src/Reader.php
@@ -2,50 +2,72 @@
 
 namespace WebGarden\Messaging;
 
+use Evenement\EventEmitter;
+use Evenement\EventEmitterInterface;
 use Redis;
 use RedisException;
+use WebGarden\Messaging\Redis\Entry;
 use WebGarden\Messaging\Redis\Stream;
 
 class Reader
 {
+    public const TIMEOUT_REACHED = 'reader.timeout_reached';
+    public const ITEM_RECEIVED = 'reader.item_received';
     public const NEW_ENTRIES = '$';
     public const PENDING_ENTRIES = '0';
 
     protected Redis $redis;
 
-    /** @var Stream[] */
+    /** @var Stream[] List of stream objects indexed by their names */
     protected array $streams;
 
-    public function __construct(Redis $redis, array $streams)
+    private EventEmitter $dispatcher;
+
+    public function __construct(Redis $redis, array $streams, ?EventEmitterInterface $dispatcher = null)
     {
         $this->redis = $redis;
-        $this->streams = $streams;
+        $this->streams = array_combine(array_map('strval', $streams), $streams);
+        $this->dispatcher = $dispatcher ?: new EventEmitter();
     }
 
-    public function follow(?string $from = null)
+    public function await(int $timeout = 0)
     {
-        $streamNames = array_map('strval', $this->streams);
-        $streams = array_fill_keys($streamNames, $from ?: self::NEW_ENTRIES);
+        $names = array_keys($this->streams);
+        $streams = array_fill_keys($names, self::NEW_ENTRIES);
+        $currentTime = fn() => hrtime(true);
 
         while (true) {
+            $start = $currentTime();
+
             try {
-                $data = $this->redis->xRead($streams, null, 1);
-
-                if (empty($data)) {
-                    $streams = array_fill_keys($streamNames, self::NEW_ENTRIES);
-                    continue;
-                }
-
-                foreach ($data as $stream => $entries) {
-                    $streams[$stream] = array_key_last($entries);
-
-                    foreach ($entries as $id => $values) {
-                        echo "Received item {$id} from {$stream} " . json_encode($values) . PHP_EOL;
-                    }
-                }
+                $data = $this->redis->xRead($streams, null, $timeout);
             } catch (RedisException $exception) {
-                echo 'Disconnected' . PHP_EOL;
+                $this->dispatcher->emit(self::TIMEOUT_REACHED, [($currentTime() - $start) / 1e+9]);
+                continue;
+            }
+
+            if (empty($data)) {
+                $streams = array_fill_keys($names, self::NEW_ENTRIES);
+                continue;
+            }
+
+            foreach ($data as $key => $entries) {
+                $streams[$key] = array_key_last($entries);
+
+                foreach ($entries as $id => $values) {
+                    $this->dispatcher->emit(self::ITEM_RECEIVED, [
+                        new Entry($id, $values),
+                        $this->streams[$key],
+                    ]);
+                }
             }
         }
+    }
+
+    public function on($event, callable $listener): self
+    {
+        $this->dispatcher->on($event, $listener);
+
+        return $this;
     }
 }
